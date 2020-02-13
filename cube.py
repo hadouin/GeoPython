c13b4c3e6ad0d4d046b97822dfb2c5d03c430327
Hadouin LEROY <hleroy@stbarth.lazo>
2020-02-13 15:21:08 +0100

    boutonleaaaaaasa

--- cube.py
+++ cube.py
@@ -28,7 +28,6 @@ bouton_quitter.grid(row = 1, column=1)
 ##----- Création du canevas -----##
 
 dessin=Canvas(root, bg='white', width=1080, height=720)
-=======
 
 ##----- Définition des Classes -----##
 class sol: 
@@ -39,12 +38,14 @@ class sol:
         dessin.create_line(0, self.y, 1100, self.y, width = 4)
 
 ##----- Définition des Fonctions -----##
+def quitter():
+    root.destroy()
 ##----- Création de la fenêtre -----##
 root = Tk()
 root.title('Géopython')
 ##----- Création des boutons -----##
-bouton_recommencer = Button(root, text='Recommencer', width=10)
-bouton_recommencer.grid()
+bouton_quitter = Button(root, text='quitter',command=quitter, width=10)
+bouton_quitter.grid(row=1,column=1)
 ##----- Création du canevas -----##
 
 dessin=Canvas(root, bg='white', width=1080, height=720)
@@ -53,9 +54,8 @@ dessin.grid(row = 0, column = 0, columnspan = 3, padx=5, pady=5)
 
 ##----- Programme principal -----##
 
-dessin.create_rectangle(515, 400, 565, 450, fill='grey', width='5')
-root.mainloop()      
-=======
+dessin.create_rectangle(515, 400, 565, 450, fill='grey', width='5')  
+
 Sol = sol()
 Sol.draw()
 
