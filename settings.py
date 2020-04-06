fc9809dfe2b2fa21ada68a24707af868e2aac04d
unknown <hadchaos@gmail.com>
2020-04-06 17:16:58 +0200

    creation du cadre principal du programme en pygame Plus quelque essais de physique

---
+++ settings.py
@@ -0,0 +1,18 @@
+#settings
+# game options/settings
+TITLE = "Geopython"
+WIDTH = 720
+HEIGHT = 480
+FPS = 60
+
+# Player proprieties
+PLAYER_ACC = 0.5
+PLAYER_FRICTION = -0.12
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
